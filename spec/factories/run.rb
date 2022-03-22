17438b9c599295a9e140d9a4e677f10b6d6e88d7
Youssef Boulkaid <youssef@yboulkaid.com>
2022-03-22 20:49:59 +0100

    Update factory

--- spec/factories/run.rb
+++ spec/factories/run.rb
@@ -1,3 +1,23 @@
 FactoryBot.define do
-  factory :run
+  factory :run do
+    node1 { 'AF-AGO' }
+    node2 { 'AF-BDI' }
+    capacity { 10 }
+    start_year { 2020 }
+    end_year { 2050 }
+    resolution do
+      {
+        day_parts: [
+          { id: 'D1', start_hour: 0, end_hour: 4 },
+          { id: 'D2', start_hour: 4, end_hour: 22 },
+          { id: 'D3', start_hour: 22, end_hour: 24 },
+        ],
+        seasons: [
+          { id: 'S1', months: [1, 2, 3, 4, 5, 6] },
+          { id: 'S2', months: [7, 8, 9, 10, 11, 12] },
+        ],
+      }
+    end
+    slug { "run-#{SecureRandom.hex(10)}" }
+  end
 end
