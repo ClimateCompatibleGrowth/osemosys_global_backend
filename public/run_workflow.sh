81b4e98d2fa475b254d59f4efb4d2572f1ea7162
Youssef Boulkaid <youssef@yboulkaid.com>
2022-03-28 22:28:00 +0200

    Add comments to bash script

--- public/run_workflow.sh
+++ public/run_workflow.sh
@@ -2,6 +2,10 @@
 set -e
 
 cd /home/ubuntu/osemosys_global/
+# To do: name argument something else than $1
 wget --output-document=/home/ubuntu/osemosys_global/config/config.yaml $1
 source /home/ubuntu/miniconda3/bin/activate osemosys-global
 snakemake -c
+# curl --request PUT \
+#   --url http://localhost:3000/runs/[slug] \
+#   --form 'result_file=@/home/ubuntu/osemosys_global/config/config.yaml'
