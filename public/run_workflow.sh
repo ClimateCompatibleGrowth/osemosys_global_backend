821ba258a98928da3431e2c992b81b7e935d6eb5
Youssef Boulkaid <youssef@yboulkaid.com>
2022-08-23 21:31:37 +0200

    Use the latest version of osemosys global

--- public/run_workflow.sh
+++ public/run_workflow.sh
@@ -17,7 +17,7 @@ cd /home/ubuntu/osemosys_global/
 
 # Pin the version
 git reset --hard && git pull
-git reset --hard 9aeedf4f
+git reset --hard 8df3389
 
 # Update conda environment
 /home/ubuntu/miniconda3/bin/conda env update -f workflow/envs/osemosys-global.yaml 
